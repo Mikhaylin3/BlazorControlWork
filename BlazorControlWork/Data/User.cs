b3d71cdbc87ed3b17348b5620e988cf24731e3f2
а б <vnmiha1@mail.ru>
2022-10-22 17:04:46 +0300

    Добавьте файлы проекта.

---
+++ BlazorControlWork/Data/User.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace BlazorControlWork.Data
+{
+    public class User
+    {
+        [BsonId]
+        [BsonIgnoreIfNull]
+        public string? Login { get; set; }
+        [BsonIgnoreIfNull]
+        public string? Password { get; set; }
+        [BsonIgnoreIfNull]
+        public string? FName { get; set; }
+        [BsonIgnoreIfNull]
+        public string? LName { get; set; }
+        [BsonIgnoreIfNull]
+        public string? Email { get; set; }
+    }
+}
---
+++ BlazorControlWork/Data/UserService.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+namespace BlazorControlWork.Data
+{
+    public class UserService
+    {
+        public User user = new User();
+
+        private static MongoClient client = new MongoClient();
+        private static IMongoDatabase database = client.GetDatabase("Davletka");
+        private IMongoCollection<User> userColl = database.GetCollection<User>("Users");
+
+        public void AddToDb(User user)
+        {
+            userColl.InsertOne(user);
+        }
+
+        public void UserLogin(string Login)
+        {
+            user = userColl.Find(x => x.Login == Login).FirstOrDefault();
+        }
+    }
+}
